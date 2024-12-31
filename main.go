0242c95e1917ff8662368d2971b951df2e8f3588
Seno Wijayanto <senowijayanto@gmail.com>
2024-12-31 21:34:16 +0700

    Initialize project. Register user endpoint

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:secret@tcp(127.0.0.1:3306)/bwastartup_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&user.User{})
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
